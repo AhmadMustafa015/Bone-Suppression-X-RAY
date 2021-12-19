eae6ffff8c114bfc29f5ab947067fe8c456b77b1
ahmad mustafa <ahmad.m.abdelqader@gmail.com>
2021-12-19 14:37:16 +0200

    fix glitches in the codes

--- Pseudo-xray.py
+++ Pseudo-xray.py
@@ -18,7 +18,7 @@ import cv2 as cv
 import os
 import tempfile
 import datetime
-
+import copy
 import pydicom
 from pydicom.dataset import FileDataset, FileMetaDataset
 class CTtoXRAY:
@@ -29,7 +29,7 @@ class CTtoXRAY:
     def __init__(self,input_dir=None):
         self.input_dir = input_dir
         self.loaded_images = []
-        self.interpolation_mode = 'linear'
+        self.interpolation_mode = 'nearest'
         self.HU_min = 205 #200 IN THE PAPER
         self.HU_max = 3000
     def load_images(self,path=None):
@@ -53,12 +53,12 @@ class CTtoXRAY:
                 scan_extension = "mhd"
                 #Add the raw mhd image to be precessed in the directory
             else:
-                print('Unknown input file format; File:\t', os.path.join(path_to_img, filename))
+                print('Unknown input file format; File:/t', os.path.join(path_to_img, filename))
         if len(slices) < 1:
-            print('No images found in the directory:\t', path_to_img)
+            print('No images found in the directory:/t', path_to_img)
 
         if scan_extension == "dcm":
-            patient_id = path_to_img.basename(path_to_img)
+            patient_id = os.path.basename(path_to_img)
             reader = sitk.ImageSeriesReader()
             itkimage = sitk.ReadImage(reader.GetGDCMSeriesFileNames(path_to_img, patient_id))
         elif scan_extension == "mhd":
@@ -79,23 +79,29 @@ class CTtoXRAY:
         HU_max: float, max HU value.
         HU_nan: float, value for nan in the raw CT image.
         """
-        CT_bone = input_image
+        CT_bone = copy.deepcopy(input_image)
+        print(CT_bone.max(),CT_bone.min())
         # Get bone only (Binarize the bones)
         CT_bone[np.where(np.logical_or(CT_bone<self.HU_min,
                                              CT_bone>self.HU_max))] = 0
-        CT_bone[np.where(np.logical_or(CT_bone>=self.HU_min,
+        CT_bone[np.where(np.logical_and(CT_bone>=self.HU_min,
                                        CT_bone<=self.HU_max))] = 1
+        print(CT_bone.max(), CT_bone.min())
         # Closing process; erosion and dilation
         kernel = np.ones((5, 5), np.uint8)
+        CT_bone = np.array(CT_bone,np.uint8)
         CT_bone_close_binary = cv.morphologyEx(CT_bone, cv.MORPH_CLOSE, kernel)
         CT_bone_close_binary[np.where(CT_bone_close_binary < 0.5)] = 0
         CT_bone_close_binary[np.where(CT_bone_close_binary >= 0.5)] = 1
         # Mask the bone
-        assert input_image.shape() == CT_bone_close_binary.shape(), "Error: bone mask size not equal CT scan size"
+        #assert np.array(input_image.shape()) == np.array(CT_bone_close_binary.shape())#, "Error: bone mask size not equal CT scan size %s, %s" %(input_image.shape(),CT_bone_close_binary.shape())
         ct_bone = input_image * CT_bone_close_binary
         return ct_bone
     def resample_image(self,input_image,spacing,new_spacing = [1.0, 1.0, 1.0]):
-        new_shape = np.round(input_image.shape * spacing / new_spacing)
+        new_spacing = np.array(new_spacing)
+        shape = np.array(input_image.shape)
+        spacing = np.array(spacing)
+        new_shape = np.round(shape * spacing / new_spacing)
 
         # the actual spacing to resample.
         resample_spacing = spacing * input_image.shape / new_shape
@@ -128,8 +134,9 @@ class CTtoXRAY:
         #equation (3)
         m_avg = (p_avg * m_w) / 1000.0 + m_w
         m_total = m_avg * Nv
-        I_u_v = math.exp(-m_total)
-        I_trans = 1/(1+np.exp(-a*(I_u_v-b*np.mean(I_u_v))))
+        I_u_v = np.exp(-m_total)
+        mean_value = np.mean(I_u_v)
+        I_trans = 1/(1+np.exp(-a*(I_u_v-b*mean_value)))
         return I_trans
     def bone_enhancement(self,image,bone,wb=1):
         """
@@ -140,7 +147,7 @@ class CTtoXRAY:
         :return: bone-enhanced image
         """
         image = image + wb * bone
-        return  image
+        return image
     def unsharp_masking_process(self,image,wu=0.7):
         """
         The unsharp mask process is a method wherein the high frequency
@@ -161,8 +168,16 @@ class CTtoXRAY:
         :param image:
         :return:
         """
-        image_min,image_max = min(image),max(image)
-        image_normalize = ((pixel-image_min)/(image_max-image_min) * 1023 for pixel in image)
+        image_min = np.min(image)
+        image_max = np.max(image)
+
+        for iz, ix in np.ndindex(image.shape):
+            pixel = image[iz, ix]
+            image[iz, ix] = (pixel - image_min) / (image_max - image_min) * 255
+        image = np.array(image,np.uint8)
+        img_path = 'F:/Radiologics/Bone Supression/Bone-Suppression-X-RAY/generated_images/1.png'
+        cv.imwrite(img_path, image)
+        """
         ##################### SAVE AS DICOM ###################################
         # Create some temporary filenames
         suffix = '.dcm'
@@ -180,7 +195,7 @@ class CTtoXRAY:
         # Create the FileDataset instance (initially no data elements, but file_meta
         # supplied)
         ds = FileDataset(filename_little_endian, {},
-                         file_meta=file_meta, preamble=b"\0" * 128)
+                         file_meta=file_meta, preamble=b"/0" * 128)
 
         # Add the data elements -- not trying to set all required here. Check DICOM
         # standard
@@ -219,18 +234,18 @@ class CTtoXRAY:
             # remove the created file
             print('Remove file {} ...'.format(filename))
             os.remove(filename)
-
+        """
 
 
 if __name__ == "__main__":
     # Step 1: Select load images and select only images with slice thickness < 1mm
     ct_to_xray = CTtoXRAY()
-    image, spacing = ct_to_xray.load_images(path='')
+    image, spacing = ct_to_xray.load_images(path='D:/LIDC-IDRI/LIDC-IDRI-0001/1.3.6.1.4.1.14519.5.2.1.6279.6001.298806137288633453246975630178/1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192')
     image = ct_to_xray.resample_image(image,spacing)
     bone_raw = ct_to_xray.extract_bones(image)
     image, bone = ct_to_xray.projection_2d(image), ct_to_xray.projection_2d(bone_raw)
-    image = ct_to_xray.bone_enhancement(image,bone)
-    image = ct_to_xray.unsharp_masking_process(image)
+    #image = ct_to_xray.bone_enhancement(image,bone)
+    #image = ct_to_xray.unsharp_masking_process(image)
     ct_to_xray.save_xray_image(image)
     print("Successfully finished")
 
